d13a5a386637b5b1f22fd30dc7f87635b72423db
Kaan240705 <as.kaan205@gmail.com>
2025-08-14 09:11:46 +0200

    Update TTField.java
    Dokumentation

--- model/TTField.java
+++ model/TTField.java
@@ -4,6 +4,7 @@ public class TTField {
     private final int index;
     private final Object parent;
 
+    // Initialisiert das Feld mit einem Index und dem übergeordneten Objekt.
     public TTField(int index, Object parent) {
         this.index = index;
         this.parent = parent;
