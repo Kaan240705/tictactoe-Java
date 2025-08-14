4ae92d417b204e54e6c88bb451b70b80b77148b8
Kaan240705 <as.kaan205@gmail.com>
2025-08-14 09:15:19 +0200

    Update HeuristicPlayer.java
    Dokumentation

--- player/HeuristicPlayer.java
+++ player/HeuristicPlayer.java
@@ -4,6 +4,11 @@ import controller.TicTacToeController;
 public class HeuristicPlayer extends ComputerPlayer {
     private final int[] prio = {4, 0, 2, 6, 8, 1, 3, 5, 7};
 
+    /*
+       Bestimmt den nächsten Zug des Computers.
+       Geht die Prioritätsliste durch und wählt das erste freie Feld.
+       Gibt -1 zurück, falls kein Feld mehr frei ist.
+    */
     @Override
     public int naechsterSpielzug(TicTacToeController controller) {
         for (int feld : prio) {
