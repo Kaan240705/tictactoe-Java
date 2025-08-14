66bf02c76a751ea91dbaad26ffebd680518db5f8
Kaan240705 <as.kaan205@gmail.com>
2025-08-14 09:40:41 +0200

    Update PlayControl.java
    Dokumentation angepasst

--- model/PlayControl.java
+++ model/PlayControl.java
@@ -1,50 +1,55 @@
 package model;
 
-    // Setzt alle Felder des Spielfelds auf leer
+/**
+ * Verwaltet das Spielfeld und die Spielregeln von Tic-Tac-Toe.
+ *
+ * Enthält ein 3x3-Array für das Spielfeld, wobei die Werte für leer,
+ * Spieler und Computer festgelegt sind. Stellt Methoden bereit, um
+ * Züge zu setzen, zu überprüfen, ob ein Feld frei ist, ob das Spiel
+ * vorbei ist, und wer gewonnen hat. Außerdem kann der aktuelle
+ * Zustand des Spielfelds abgefragt werden.
+ */
+
+public class PlayControl {
+    private final int[][] pField = new int[3][3];
+    private final int none = 0, player = 1, computer = 2;
+
     public void reset() {
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
                 pField[i][j] = none;
     }
 
-    // Setzt das Feld an der angegebenen Position für den Spieler
     public void playerSet(int nr) {
         setPoint(nr, player);
     }
 
-    // Setzt das Feld an der angegebenen Position für den Computer
     public void computerSet(int nr) {
         setPoint(nr, computer);
     }
 
-    // Setzt ein Feld an der gegebenen Position mit dem angegebenen Typ (Spieler oder Computer)
     private void setPoint(int pos, int type) {
         int row = pos / 3, col = pos % 3;
         pField[row][col] = type;
     }
 
-    // Prüft, ob das Feld an der angegebenen Position frei ist
     public boolean fieldFree(int nr) {
         int row = nr / 3, col = nr % 3;
         return pField[row][col] == none;
     }
 
-    // Prüft, ob das Spiel vorbei ist (Sieg oder volles Spielfeld)
     public boolean gameOver() {
         return winnerIs() != 0 || isFull();
     }
 
-    // Prüft, ob der Spieler gewonnen hat
     public boolean playerHasWon() {
         return winnerIs() == player;
     }
 
-    // Prüft, ob der Computer gewonnen hat
     public boolean computerHasWon() {
         return winnerIs() == computer;
     }
 
-    // Prüft, ob das Spielfeld voll ist
     private boolean isFull() {
         for (int[] row : pField)
             for (int cell : row)
@@ -52,7 +57,6 @@ package model;
         return true;
     }
 
-    // Prüft, ob es einen Gewinner gibt und gibt den Typ (Spieler oder Computer) zurück, sonst 0
     private int winnerIs() {
         for (int i = 0; i < 3; i++) {
             if (pField[i][0] != 0 && pField[i][0] == pField[i][1] && pField[i][1] == pField[i][2])
@@ -67,7 +71,6 @@ package model;
         return 0;
     }
 
-    // Gibt das aktuelle Spielfeld zurück
     public int[][] getBoard() {
         return pField;
     }
