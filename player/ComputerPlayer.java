5beb35f41ac6717d5cf865a394c8091207355538
Kaan240705 <as.kaan205@gmail.com>
2025-08-14 09:13:13 +0200

    Update ComputerPlayer.java
    Dokumentation

--- player/ComputerPlayer.java
+++ player/ComputerPlayer.java
@@ -1,6 +1,11 @@
 package player;
 import controller.TicTacToeController;
 
-public abstract class ComputerPlayer {
-    public abstract int naechsterSpielzug(TicTacToeController controller);
-}
+    /*
+    Bestimmt den nächsten Zug des Computers.
+    Erwartet den aktuellen Spielzustand über einen TicTacToeController.
+    Gibt die Position (0–8) zurück, die der Computer setzen möchte.
+    */
+    public abstract class ComputerPlayer {
+        public abstract int naechsterSpielzug(TicTacToeController controller);
+    }
