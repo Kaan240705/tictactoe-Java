232cc0e188ed1b8b2716771a99bc2a6f76e64d58
Kaan240705 <as.kaan205@gmail.com>
2025-08-14 09:16:55 +0200

    Update VerySimplePlayer.java
    Dokumentation

--- player/VerySimplePlayer.java
+++ player/VerySimplePlayer.java
@@ -5,6 +5,10 @@ import java.util.Random;
 public class VerySimplePlayer extends ComputerPlayer {
     private final Random rand = new Random();
 
+    /*
+       Bestimmt den nächsten Zug des Computers.
+       Wählt zufällig ein freies Feld (0–8) aus, bis ein gültiger Zug gefunden wird.
+    */
     @Override
     public int naechsterSpielzug(TicTacToeController controller) {
         int zug;
